c131c1b83bfda0efbb3459cbdff7fe478628a675
philopon <philopon.dependence@gmail.com>
2015-03-05 05:30:58 +0900

    fix cabal, example

--- examples/example.hs
+++ examples/example.hs
@@ -4,4 +4,4 @@ import Graphics.Badge.Barrier
 import qualified Data.ByteString.Lazy.Char8 as L
 
 main :: IO ()
-main = L.putStrLn $ renderBadge (plastic & right .~ red) "build" "failing"
+main = L.putStrLn $ renderBadge (flat & right .~ red) "build" "failing"
